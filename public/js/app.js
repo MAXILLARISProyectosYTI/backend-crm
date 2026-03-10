bb71d7201389d3c265ac84be6855cbbc691f72f0
MAXILLARISProyectosYTI <135283519+MAXILLARISProyectosYTI@users.noreply.github.com>
2026-03-10 13:56:16 -0500

    Update server and websocket URLs to production

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 // Configuración
 const CONFIG = {
-    serverUrl: 'http://161.132.211.235:8993',
-    websocketUrl: 'ws://161.132.211.135:8993',
+    serverUrl: 'https://api.crm.maxillaris.pe',
+    websocketUrl: 'wss://api.crm.maxillaris.pe',
     // serverUrl: 'http://161.132.211.235:8993',
     // websocketUrl: 'ws://161.132.211.235:8993',
     reconnectAttempts: 5,
