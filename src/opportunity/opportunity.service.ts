d540b0bfe1cf3dcbbe56217b23ec68edc143788b
Bryan Simeon <bryansonva2018@gmail.com>
2026-08-03 13:00:24 -0500

    feat: add relation to assignedUserId in redirectToManager method
    - Updated the redirectToManager method to include the 'assignedUserId' relation in the opportunity retrieval query, ensuring that the assigned user information is available when processing opportunities.
    - This change enhances the functionality of the method by providing necessary user context for better decision-making in opportunity management.

--- src/opportunity/opportunity.service.ts
+++ src/opportunity/opportunity.service.ts
@@ -3445,6 +3445,7 @@ export class OpportunityService {
   async redirectToManager(usuario: string, opportunityId: string, isOiDerivedFlow?: boolean) {
     const opportunity = await this.opportunityRepository.findOne({
       where: { id: opportunityId, deleted: false },
+      relations: ['assignedUserId'],
     });
     if (!opportunity) {
       throw new NotFoundException("No se encontró la oportunidad");
